e1472239e5b4143dfd52eb96f239bf8d4f2041e8
AnSingh1 <130875753+AnSingh1@users.noreply.github.com>
2023-08-06 18:47:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import firebase_admin
 from firebase_admin import db, credentials
@@ -10,8 +9,9 @@ import os
 
 load_dotenv()
 openai.api_key = os.environ.get('API_KEY')
+openai.api_base = os.environ.get('API_BASE')
 
-cred = credentials.Certificate("key.json")
+cred = credentials.Certificate(json.loads(os.environ.get("FIREBASE_KEY")))
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://lexily-9bc6b-default-rtdb.firebaseio.com/'})
 
 difficulty_desc = {10: "give extremely tough questions. Use complex words for the question and passage. Make the passage about something extremely niche.", 9: "make the passage about something niche, use complex words for the passage but semi-complex words for the questions. The questions must require critical thinking. ", 8: "Make the passage about something very niche, use complex words for the passage but medium complexity words for the questions. Questions should require critical thinking.", 7: "make the passage about something very niche and use complex words for the passage. Questions should require critical thinking.", 6:"make the passage about something niche and require the passage to be fully understood to be able to understand the questions. Use semi-complex words for the passage.", 5:"make the passage niche and require a complex understanding of the passage to answer the questions.", 4:"make the passage unique and require understanding in order to answer the questions.", 3: "make the passage semi-unique, the user should not be able to guess the questions right.", 2: "use complex words for the questions.", 1: "use simple terms and a generic passage.", 0: "use simple terms for everything and give a very generic passage."}
@@ -46,16 +46,14 @@ def generate():
     
     Provide a passage relating to the provided prompt within the JSON object. It should be created with the following format: "passage": <passage>, where <passage> is to be replaced with the generated passage. Be sure to include a comma after the passage to follow the json format.
     Provide a title for the passage. It should be created with the following format: "title": <title>, where <title> is to be replaced with the generated title.
-    Provide a subtitle for the passage. It should be a more specific overview of the passage than the title. It should be no more than 50 characters in length. It should be created with the following format: "subtitle": <subtitle>, where <subtitle> is to be replaced with the generated subtitle.
-    Provide an array of four objects, each of which will contain a question relating to the passage. Within each object, provide a question of the same difficulty as the passage. It should be created with the following format: "question": <question>, where <question> is to be replaced with the generated question.
-    Within each question object, provide an array of 4 potential answers to the object's question. There should be only one correct answer. It should be created with the following format: "options": [<option0>, <option1>, <option2>, <option3>], where each item in the array is to be replaced with one of the generated options. Do not include any extra characters in each option.
-    Within each question object, provide the index of the correct answer to the question. It should be either 0, 1, 2, or 3. It should be created with the following format: "answer": <answer>, where <answer> is to be replaced with the index of the correct answer. 
-
-    MAKE SURE TO PLACE A COMMA SEPARATING EACH KEY-VALUE PAIR.
-
+    Provide a subtitle for the passage. It should be a more specific overview of the passage than the title. It should be created with the following format: "subtitle": <subtitle>, where <subtitle> is to be replaced with the generated subtitle.
+    Provide four multiple choice question for the passage. The question should reference the content of the passage. The question should be of the same difficulty as the passage. It should be created with the following format: "question": <question>, where <question> is to be replaced with the generated question.
+    Provide an array of four options for each question, 16 options total. Only one should be the correct answer. It should be created with the following format: "options": [<option0>, <option1>, <option2>, <option3>], where each item in the array is to be replaced with one of the generated options.
+    Provide the index of the correct answer in the questions array. It should be either 0, 1, 2, or 3. It should be created with the following format: "answer": <answer>, where <answer> is to be replaced with the index of the correct answer.
+    
     Here is an example of a proper response:
     
-    {
+     {
         "title": "Some Title",
         "subtitle": "A subtitle",
         "passage": "Paragraph 1. \n\nParagraph 2",
@@ -102,26 +100,34 @@ def generate():
             },
         ]
     }
+    
     """
 
     initial = f"""The user has done {data['numTests']} tests and wants a passage with difficulty of {data['difficulty']}/10. The theme preferred is {data['theme']} This means that you MUST {difficulty_desc[int(data['difficulty'])]}"""
 
     messages = [{"role": "system", "content": instruction}, {"role": "user", "content": initial}]
 
-    questions_data = None
+    try:
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = messages
+        )
+        result = response["choices"][0]["message"]["content"]
+        print("Result")
+        print(result)
 
-    while not questions_data:
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-            )
-            result = response["choices"][0]["message"]["content"]
+        questions_data = json.loads(result, strict=False)
+        questions_data["difficulty"] = int(data["difficulty"])
 
-            questions_data = json.loads(result, strict=False)
-            questions_data["difficulty"] = int(data["difficulty"])
-        except Exception as e:
-            print(e)
+
+
+
+
+
+    except Exception as e:
+        print(e)
+        return jsonify({"message": e})
+
 
     print(questions_data)
     return jsonify(questions_data)
